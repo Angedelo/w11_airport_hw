dfb4efe60bcf801eec59e359567d7f17d74f2e29
Matt Addison <matthew.addison@talk21.com>
2018-10-14 15:03:05 +0100

    flight tested

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,24 @@
 public class Flight {
+    private int flightNum;
+    private String destination;
+    private Plane plane;
+
+    public Flight(int flightNum, String destination){
+        this.flightNum = flightNum;
+        this.destination = destination;
+        this.plane = null;
+    }
+
+
+    public int getFlightNum() {
+        return this.flightNum;
+    }
+
+    public String getDestination() {
+        return this.destination;
+    }
+
+    public Plane addPlane(Plane planeOne) {
+        return this.plane = planeOne;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+    Plane planeOne;
+    Flight flightOne;
+
+    @Before
+    public void before(){
+        planeOne = new Plane(Type.BOEING747, "KLM");
+        flightOne = new Flight(001, "Chicago");
+    }
+
+    @Test
+    public void hasFlightNum(){
+        assertEquals(001, flightOne.getFlightNum());
+    }
+    @Test
+    public void hasDestination(){
+        assertEquals("Chicago", flightOne.getDestination());
+    }
+    @Test
+    public void canAddPlaneToFlight(){
+        assertEquals(planeOne, flightOne.addPlane(planeOne));
+    }
+
 }

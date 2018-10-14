ec4e57ae2b068f1f4cf7e3891741f81115cbf60b
Matt Addison <matthew.addison@talk21.com>
2018-10-14 15:14:00 +0100

    ticket tested

--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,2 +1,18 @@
 public class Ticket {
+    private Passenger passenger;
+    private Flight flight;
+
+    public Ticket(Passenger passenger, Flight flight){
+        this.passenger = passenger;
+        this.flight = flight;
+    }
+
+
+    public Flight getFlight() {
+        return this.flight;
+    }
+
+    public Passenger getPassenger() {
+        return this.passenger;
+    }
 }
--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TicketTest {
+    Ticket ticket;
+    Flight flight;
+    Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Alice Rees", "Bhutan");
+        flight = new Flight(001, "Chicago");
+        ticket = new Ticket(passenger, flight);
+    }
+
+    @Test
+    public void hasFlight(){
+        assertEquals(flight, ticket.getFlight());
+    }
+
+    @Test
+    public void hasPassenger(){
+        assertEquals(passenger, ticket.getPassenger());
+    }
 }
